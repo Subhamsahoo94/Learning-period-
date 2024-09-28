d6dac63814a1e921b9400ab6de6b698578d57042
Subham sahoo <subhamsahoo0p90@gmail.com>
2024-09-29 03:09:04 +0530

    Add files via upload
    This is a coding for swaping two numbers without using third variable

---
+++ swap2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <conio.h>
+  
+  int main()
+  
+{  
+   int x,y;
+   clrscr();
+   printf("enter frist noumber: ");
+   scanf("%d",&x);
+   printf("enter second number: ");
+   scanf("%d",&y);
+   x=x+y;
+   y=x-y;
+   x=x-y;
+   printf("\t_________***************_________ \n");
+   printf;("after swaping the new are :");
+   printf("frist number:  %d \n",x);
+   printf("second number: %d \n",y);
+
+}  

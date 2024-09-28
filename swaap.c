f59b22f5fcedeaa2088ca9a3c2e148b13cf4ede5
Subhamsahoo94 <subhamsahoo0p90@gmail.com>
2024-09-28 15:43:56 +0530

    Add files via upload

---
+++ swaap.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <conio.h>
+  
+  int main()
+  
+{  
+   int a,b,c;
+   clrscr();
+   printf("enter frist noumber ");
+   scanf("%d",&a);
+   printf("enter second number ");
+   scanf("%d",&b);
+   c=a;
+   a=b;
+   b=c;
+   printf("\t_________***************_________ \n");
+   printf;("after swaping the new are :");
+   printf("frist number %d \n",a);
+   printf("second number %d \n",b);
+
+}  
